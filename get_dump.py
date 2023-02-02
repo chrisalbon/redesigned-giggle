51b03cb239996482258d8aa1fc15ef5fd3ebbe19
Chris Albon <chris.albon@gmail.com>
2023-02-02 07:54:49 -0800

    checkpoint:working extractor and decompressor

---
+++ get_dump.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+
+DUMP_URL = "https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2"
+BZ2_FILE = "enwiki-latest-pages-articles.xml.bz2"
+
+# Download the latest dump of the English Wikipedia
+def download_dump(url, filename):
+    """Download the latest dump of the English Wikipedia"""
+
+    response = requests.get(url, stream=True)
+
+    # Get the total size in bytes for progress bar
+    total_length = int(response.headers.get('Content-Length', 0))
+    block_size = 1024
+    written = 0
+
+    with open(filename, "wb") as file:
+        for data in response.iter_content(block_size):
+            # Update progress bar
+            written += len(data)
+            # Write data to file
+            file.write(data)
+            # Print progress
+            done = int(50 * written / total_length)
+            sys.stdout.write("\r[{}{}] {:.1f}%".format(
+                '=' * done, ' ' * (50-done), 100 * written / total_length))
+            sys.stdout.flush()
+    print()
+
+if __name__ == "__main__":
+    download_dump(DUMP_URL, BZ2_FILE)
+
+    # Hacky way to run WikiExtractor
+    os.system('python -m wikiextractor.WikiExtractor enwiki-latest-pages-articles.xml.bz2')
